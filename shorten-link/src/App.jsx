6b3b56e643ebe3ba6311f345c2743d17738be1d1
CHATL <chatchawal2547@gmail.com>
2024-11-28 00:41:19 +0700

    adding route lauout outlet

--- shorten-link/src/App.jsx
+++ shorten-link/src/App.jsx
@@ -1,10 +1,14 @@
 import Form from "./components/Form";
+import Login from "./components/Login";
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <>
-      <div className="mt-36 px-2 sm:px-4 md:px-8 lg:px-16 xl:px-24 2xl:px-32 text-lg h-screen flex justify-center items-start">
-        {/* <Layout /> */}
+      <div className="px-2 sm:px-4 md:px-8 lg:px-16 xl:px-24 2xl:px-32 text-lg h-screen flex flex-col items-center justify-start gap-8">
+        <NavBar />
+        {/* Outlet */}
         <Form />
+        {/* <Login /> */}
       </div>
     </>
   );
