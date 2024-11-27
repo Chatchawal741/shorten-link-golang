432517c364072a8cfd7a4411c0320abe466e4884
CHATL <chatchawal2547@gmail.com>
2024-11-28 00:41:33 +0700

    adding login form

--- shorten-link/src/components/Form.jsx
+++ shorten-link/src/components/Form.jsx
@@ -2,15 +2,15 @@ import Input from "./Input";
 
 function Form() {
   return (
-    <div className="mt-4 flex flex-col items-center justify-center">
+    <div className="h-[40vh] flex flex-col items-center justify-center">
       <div>
-        <label
+        {/* <label
           htmlFor="short_link"
           className="text-2xl font-semibold text-green-600 sha"
         >
           shorten link
-        </label>
-        <div className="md:w-[calc(100vw/2)] p-4 bg-green-800  rounded-2xl justify-between md:rounded-lg mt-4 flex flex-col gap-6 md:flex-row md:gap-4 shadow-xl shadow-gray-300 text-white">
+        </label> */}
+        <div className="md:w-[calc(100vw/2)] p-4 bg-green-800  rounded-2xl justify-between md:rounded-lg flex flex-col gap-6 md:flex-row md:gap-4 shadow-xl shadow-gray-300 text-white">
           <Input />
         </div>
       </div>
